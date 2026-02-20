4350c511d83c9edb3ceb23e398dfecbc1f1fe221
naomiven <naomi.mira@gmail.com>
2026-02-20 00:20:41 -0500

    parse output and return dictionary

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,7 @@
+import json
 import requests
 
-"""
-Input json: { "raw_document": { "text": text_to_analyse } }
-"""
+
 URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
 def emotion_detector(text_to_analyze):
@@ -18,4 +17,23 @@ def emotion_detector(text_to_analyze):
         }
     )
 
-    return response.text
+    json_response = json.loads(response.text)
+
+
+    emotions = json_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
